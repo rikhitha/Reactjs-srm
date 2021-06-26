1b99116ae985196ebaff74ee992f59ca5de2c66f
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-06-26 11:54:44 +0530

    Create NewStateless.js

---
+++ PropsAndState/components/NewStateless.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewRegis from'./NewRegis';
+
+export default function (props) {
+    return (
+        <div>
+            the Answer is {props.ans}
+        </div>
+    )
+}
