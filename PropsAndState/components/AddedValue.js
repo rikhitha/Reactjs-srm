0cf023ec02890490c59ec2313ddffd51f84809c4
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-06-26 11:52:27 +0530

    Create AddedValue.js

---
+++ PropsAndState/components/AddedValue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Addition from './Addition';
+
+export default function AddedValue(props) {
+    return (
+        <div>
+            Added value is {props.ans}
+        </div>
+    )
+}
